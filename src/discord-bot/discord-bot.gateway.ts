10ac1379739f017647f8896ab24af54d1254588b
Kim LeeJun <hwanam1111@naver.com>
2023-03-25 21:39:10 +0900

    feat: bot activity

--- src/discord-bot/discord-bot.gateway.ts
+++ src/discord-bot/discord-bot.gateway.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Once, InjectDiscordClient } from '@discord-nestjs/core';
-import { Client } from 'discord.js';
+import { Client, ActivityType } from 'discord.js';
 
 @Injectable()
 export class DiscordBotGateway {
@@ -11,6 +11,7 @@ export class DiscordBotGateway {
 
   @Once('ready')
   onReady() {
+    this.client.user.setActivity('명령어는 "/팀나누기"', { type: ActivityType.Playing });
     console.log(`Discord Bot ${this.client.user.tag} was started!`);
   }
 }
